26bd598651e3ab6adefb3bcc0d09085e1a1fe9ab
Shikhar Vishwakarma <shikharvishwa123@gmail.com>
2026-01-31 20:36:11 +0530

    Add AppConstants class with API keys and coordinates

---
+++ lib/core/constrants.dart
@@ -0,0 +1,9 @@
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class AppConstants {
+  static const String googleMapsKey = "YOUR GOOGLE MAPS API KEY";
+  static const String geminiApiKey = "YOUR GEMINI API KEY";
+  
+  // Raipur Center Coordinates for my city (can be for your)
+  static const LatLng raipurCenter = LatLng(21.2514, 81.6296);
+}
